21186ff3043ccb6f1c501b4b42ab5270b7fec919
JeremTer <ternisien09@gmail.com>
2021-05-10 21:24:07 +0200

    Refacto work sql entity

--- src/main/java/com/gotta_watch_them_all/app/work/infrastructure/dataprovider/entity/WorkEntity.java
+++ src/main/java/com/gotta_watch_them_all/app/work/infrastructure/dataprovider/entity/WorkEntity.java
@@ -1,10 +1,12 @@
 package com.gotta_watch_them_all.app.work.infrastructure.dataprovider.entity;
 
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity(name = "work")
 @NoArgsConstructor
@@ -15,15 +17,48 @@ public class WorkEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "imdb_id")
+    private String imdbId;
+
     @Column
     private String title;
 
     @Column
-    private String description;
+    private String year;
 
     @Column
-    private String author;
+    private LocalDate releasedDate;
+
+    @Column
+    private Integer duration;
+
+    @Column
+    private String genres;
+
+    @Column
+    private String directors;
+
+    @Column(columnDefinition = "TEXT")
+    private String writers;
+
+    @Column
+    private String actors;
+
+    @Column(columnDefinition = "TEXT")
+    private String plot;
+
+    @Column
+    private String country;
+
+    @Column
+    private String awards;
+
+    @Column(columnDefinition = "TEXT")
+    private String poster;
 
     @Column(name = "media_id")
     private Long mediaId;
+
+    @Column
+    private Integer score;
 }
