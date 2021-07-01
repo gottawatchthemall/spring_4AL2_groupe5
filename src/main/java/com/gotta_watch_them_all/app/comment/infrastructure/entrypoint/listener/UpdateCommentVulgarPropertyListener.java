a67798d7caf55aa53e13b5c0251d9908e3a9fe6a
MasatakaIshii <masaradio78@gmail.com>
2021-07-02 00:33:58 +0200

    UpdateCommentVulgarPropertyListener unit tested

--- src/main/java/com/gotta_watch_them_all/app/comment/infrastructure/entrypoint/listener/UpdateCommentVulgarPropertyListener.java
+++ src/main/java/com/gotta_watch_them_all/app/comment/infrastructure/entrypoint/listener/UpdateCommentVulgarPropertyListener.java
@@ -1,13 +1,18 @@
 package com.gotta_watch_them_all.app.comment.infrastructure.entrypoint.listener;
 
 import com.gotta_watch_them_all.app.comment.core.event.UpdateCommentVulgarPropertyEvent;
+import com.gotta_watch_them_all.app.comment.usecase.UpdateCommentVulgarProperty;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UpdateCommentVulgarPropertyListener implements ApplicationListener<UpdateCommentVulgarPropertyEvent> {
+    private final UpdateCommentVulgarProperty updateCommentVulgarProperty;
+
     @Override
     public void onApplicationEvent(UpdateCommentVulgarPropertyEvent event) {
-        System.out.println("test");
+        updateCommentVulgarProperty.execute();
     }
 }
