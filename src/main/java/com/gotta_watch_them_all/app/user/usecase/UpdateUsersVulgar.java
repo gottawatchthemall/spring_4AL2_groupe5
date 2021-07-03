767899314480ae6ca4a75da38a5ef2358c8871d4
MasatakaIshii <masaradio78@gmail.com>
2021-07-03 20:07:06 +0200

    UpdateUsersVulgarListener unit tested

---
+++ src/main/java/com/gotta_watch_them_all/app/user/usecase/UpdateUsersVulgar.java
@@ -0,0 +1,12 @@
+package com.gotta_watch_them_all.app.user.usecase;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UpdateUsersVulgar {
+    public void execute(Set<Long> setUserId) {
+        
+    }
+}
