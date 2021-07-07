9f37b06ac76c58cb627791a7d620df6620a2e5c5
MasatakaIshii <masaradio78@gmail.com>
2021-07-07 19:40:19 +0200

    UserController WIP
    Prepare FindAllUser

---
+++ src/main/java/com/gotta_watch_them_all/app/user/core/dto/DtoUser.java
@@ -0,0 +1,4 @@
+package com.gotta_watch_them_all.app.user.core.dto;
+
+public class DtoUser {
+}
---
+++ src/main/java/com/gotta_watch_them_all/app/user/usecase/FindAllUser.java
@@ -0,0 +1,13 @@
+package com.gotta_watch_them_all.app.user.usecase;
+
+import com.gotta_watch_them_all.app.user.core.dto.DtoUser;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class FindAllUser {
+    public Set<DtoUser> execute(Boolean isVulgar) {
+        return null;
+    }
+}
