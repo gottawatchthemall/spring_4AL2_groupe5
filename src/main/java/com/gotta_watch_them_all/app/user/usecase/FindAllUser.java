b7e1cfcdd2db22612792eab7ae2df4a0b224be88
MasatakaIshii <masaradio78@gmail.com>
2021-07-07 20:12:37 +0200

    FindAllUser usecase done

--- src/main/java/com/gotta_watch_them_all/app/user/usecase/FindAllUser.java
+++ src/main/java/com/gotta_watch_them_all/app/user/usecase/FindAllUser.java
@@ -2,16 +2,23 @@ package com.gotta_watch_them_all.app.user.usecase;
 
 import com.gotta_watch_them_all.app.user.core.dao.UserDao;
 import com.gotta_watch_them_all.app.user.core.dto.DtoUser;
+import com.gotta_watch_them_all.app.user.infrastructure.entrypoint.adapter.UserAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FindAllUser {
     private final UserDao userdao;
     public Set<DtoUser> execute(Boolean isVulgar) {
-        return null;
+        var setUser = userdao.findAll();
+
+        return setUser.stream()
+                .filter(user -> user.isVulgar() == isVulgar)
+                .map(UserAdapter::domainToDto)
+                .collect(Collectors.toSet());
     }
 }
