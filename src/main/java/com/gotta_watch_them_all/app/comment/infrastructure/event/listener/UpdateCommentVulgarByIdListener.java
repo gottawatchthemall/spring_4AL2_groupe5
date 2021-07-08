dc5445e1d2742549cbe5936cdfeb1b31f6efde2f
MasatakaIshii <masaradio78@gmail.com>
2021-07-08 22:50:18 +0200

    UpdateCommentVulgarByIdListener done

--- src/main/java/com/gotta_watch_them_all/app/comment/infrastructure/event/listener/UpdateCommentVulgarByIdListener.java
+++ src/main/java/com/gotta_watch_them_all/app/comment/infrastructure/event/listener/UpdateCommentVulgarByIdListener.java
@@ -2,17 +2,23 @@ package com.gotta_watch_them_all.app.comment.infrastructure.event.listener;
 
 import com.gotta_watch_them_all.app.comment.core.event.UpdateCommentVulgarByIdEvent;
 import com.gotta_watch_them_all.app.comment.usecase.UpdateCommentVulgarById;
+import com.gotta_watch_them_all.app.user.core.event.UpdateUsersVulgarEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class UpdateCommentVulgarByIdListener implements ApplicationListener<UpdateCommentVulgarByIdEvent> {
     private final UpdateCommentVulgarById updateCommentVulgarById;
+    private final UpdateUsersVulgarEventPublisher updateUsersVulgarEventPublisher;
 
     @Override
     public void onApplicationEvent(UpdateCommentVulgarByIdEvent event) {
-        updateCommentVulgarById.execute(event.getCommentId());
+        var updateComment = updateCommentVulgarById.execute(event.getCommentId());
+        var setUserId = Set.of(updateComment.getUserId());
+        updateUsersVulgarEventPublisher.publishEvent(setUserId);
     }
 }
--- src/test/java/com/gotta_watch_them_all/app/unit/comment/infrastructure/event/listener/UpdateCommentVulgarByIdListenerTest.java
+++ src/test/java/com/gotta_watch_them_all/app/unit/comment/infrastructure/event/listener/UpdateCommentVulgarByIdListenerTest.java
@@ -1,39 +1,49 @@
 package com.gotta_watch_them_all.app.unit.comment.infrastructure.event.listener;
 
+import com.gotta_watch_them_all.app.comment.core.entity.Comment;
 import com.gotta_watch_them_all.app.comment.core.event.UpdateCommentVulgarByIdEvent;
 import com.gotta_watch_them_all.app.comment.infrastructure.event.listener.UpdateCommentVulgarByIdListener;
 import com.gotta_watch_them_all.app.comment.usecase.UpdateCommentVulgarById;
+import com.gotta_watch_them_all.app.user.core.event.UpdateUsersVulgarEventPublisher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateCommentVulgarByIdListenerTest {
-    private final long commentId = 12L;
     private UpdateCommentVulgarByIdListener sut;
 
     @Mock
     private UpdateCommentVulgarById mockUpdateCommentVulgarById;
 
+    @Mock
+    private UpdateUsersVulgarEventPublisher mockUpdateUsersVulgarEventPublisher;
+
     @BeforeEach
     void setup() {
-        sut = new UpdateCommentVulgarByIdListener(mockUpdateCommentVulgarById);
-    }
-
-    @Test
-    void should_call_usecase_update_comment_vulgar_by_id() {
-        sut.onApplicationEvent(new UpdateCommentVulgarByIdEvent(sut, commentId));
-
-        verify(mockUpdateCommentVulgarById, times(1)).execute(commentId);
+        sut = new UpdateCommentVulgarByIdListener(mockUpdateCommentVulgarById, mockUpdateUsersVulgarEventPublisher);
     }
 
     @Test
     void when_call_usecase_should_publish_update_users_vulgar() {
+        long commentId = 12L;
+        var comment = new Comment()
+                .setId(commentId)
+                .setVulgar(true)
+                .setContent("content")
+                .setWorkId(16L)
+                .setUserId(11L)
+                .setUsername("username");
+        when(mockUpdateCommentVulgarById.execute(commentId)).thenReturn(comment);
 
+        sut.onApplicationEvent(new UpdateCommentVulgarByIdEvent(sut, commentId));
+
+        verify(mockUpdateUsersVulgarEventPublisher, times(1)).publishEvent(Set.of(comment.getUserId()));
     }
 }
